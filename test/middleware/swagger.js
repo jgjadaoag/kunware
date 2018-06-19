dafbd17520121dc5fde63594e6d619cb708ec9cb
Joseph Gabriel J. Adaoag <jgj.adaoag@gmail.com>
2018-06-19 11:57:12 +0800

    Add watch feature for kunware

---
+++ test/middleware/swagger.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const rewire = require('rewire');
+const sinon = require('sinon');
+const swagger = rewire('../../lib/middleware/swagger');
+
+describe('The swagger middleware', function() {
+  describe('watcher', function() {
+    xit('should create a new swagger router on file change', function() {
+
+    });
+
+    xit('should watch multiple files and directories', function() {
+
+    });
+
+    xit('should not create a new router on unmodified file', function() {
+
+    });
+  });
+})
+;
